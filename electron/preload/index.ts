a458cc181d5f61b52a52769a738327f9367ee6dc
wuda <1466635512@qq.com>
2024-12-10 11:40:26 +0800

    Initial commit

---
+++ electron/preload/index.ts
@@ -0,0 +1,2 @@
+// 这里可以添加需要在渲染进程中使用的API
+console.log('preload script loaded')
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
